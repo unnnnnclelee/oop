36160222c476649fabcc48413846853a2ff211e3
unnnnnclelee <1742853030@qq.com>
2018-10-08 12:53:45 +0800

    Initial commit

---
+++ code/01.py
@@ -0,0 +1,11 @@
+class Student():
+    name = None
+    age = 18
+    def DoHomework(self):
+        print("我已经在做了")
+
+
+naotou=Student()
+print(naotou.age)
+print(naotou.name)
+naotou.DoHomework()
